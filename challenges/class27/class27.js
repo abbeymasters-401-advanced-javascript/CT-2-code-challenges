9754796d087dabf50a3c6ba3373c5219674f8826
Abbey Masters <abbeymasters@Abbeys-MacBook-Pro.local>
2019-10-29 20:13:12 -0700

    fixing functions

--- challenges/class27/class27.js
+++ challenges/class27/class27.js
@@ -1,12 +1,14 @@
 const arr = [3, 6, 7, 10, 40, 32, 1];
-console.log(mergeSort(arr));
+console.log(arr);
+mergeSort(arr);
+console.log(arr);
 
 function mergeSort(arr) {
-  const n = arr.length;
-  if(n > 1) {
+  let n = arr.length;
+  if (n > 1) {
     let mid = Math.floor(n / 2);
     let left = arr.slice(0, mid);
-    let right = arr.slice(mid + 1, n - 1);
+    let right = arr.slice(mid, n);
 
     mergeSort(left);
     mergeSort(right);
@@ -14,27 +16,13 @@ function mergeSort(arr) {
   }
 }
 
-// ALGORITHM Mergesort(arr)
-//     DECLARE n <-- arr.length
-           
-//     if n > 1
-//       DECLARE mid <-- n/2
-//       DECLARE left <-- arr[0...mid]
-//       DECLARE right <-- arr[mid...n]
-//       // sort the left side
-//       Mergesort(left)
-//       // sort the right side
-//       Mergesort(right)
-//       // merge the sorted left and right sides together
-//       Merge(left, right, arr)
-
 function merge(left, right, arr) {
   let i = 0;
   let j = 0;
   let k = 0;
 
-  while(i < left.length && j < right.length) {
-    if(left[i] <= right[j]) {
+  while (i < left.length && j < right.length) {
+    if (left[i] <= right[j]) {
       arr[k] = left[i];
       i++;
     } else {
@@ -43,29 +31,17 @@ function merge(left, right, arr) {
     }
     k++;
   }
-  if(i === left.length) {
-    arr.concat(right.slice(j));
+  if (i === left.length) {
+    while (j < right.length) {
+      arr[k] = right[j];
+      j++;
+      k++;
+    }
   } else {
-    arr.concat(left.slice(i));
+    while (i < left.length) {
+      arr[k] = left[i];
+      i++;
+      k++;
+    }
   }
 }
-
-// ALGORITHM Merge(left, right, arr)
-//     DECLARE i <-- 0
-//     DECLARE j <-- 0
-//     DECLARE k <-- 0
-
-//     while i < left.length && j < right.length
-//         if left[i] <= right[j]
-//             arr[k] <-- left[i]
-//             i <-- i + 1
-//         else
-//             arr[k] <-- right[j]
-//             j <-- j + 1
-            
-//         k <-- k + 1
-
-//     if i = left.length
-//        set remaining entries in arr to remaining values in right
-//     else
-//        set remaining entries in arr to remaining values in left
